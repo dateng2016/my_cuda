1eae28c5aca7ee734ac52951ef1926840a86d7b4
Da Teng <dateng2016@gmail.com>
2024-11-08 17:52:09 -0500

    a

--- CUDA_SFML/src/cuda_kernels.cu
+++ CUDA_SFML/src/cuda_kernels.cu
@@ -62,10 +62,10 @@ void normalMemSimulate(RenderWindow& window, int threadsPerBlock,
     // cudaMalloc(&d_gridNext, size);
     // cudaMallocManaged(&d_gridCurrent, size);
     // cudaMallocManaged(&d_gridNext, size);
-    cudaMallocHost(&d_gridCurrent, size);
-    cudaMallocHost(&d_gridNext, size);
+    // cudaMallocHost(&d_gridCurrent, size);
+    // cudaMallocHost(&d_gridNext, size);
 
-        // * Flatten the vectors
+    // * Flatten the vectors
     vector<uint8_t> flatGridCurrent;
     vector<uint8_t> flatGridNext;
     flatGridCurrent.reserve(gridWidth *
